bdc7dc4235fe9d21687970a67df9c80d5d042e40
Laura Nguyen <hello@lauranguyen.me>
2024-08-18 23:44:13 -0400

    added assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
+import illustration from "./../assets/images/illustration-sign-up-mobile.svg";
 export default function Home() {
+  function handleSubmit() {}
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <img src={illustration} alt="sign up mobile" />
+      <h1>Stay updated!</h1>
+      <p>Join 60,000+ product managers receiving monthly updates on:</p>
+      <ul>
+        <li>Product discovery and building what matters</li>
+        <li>Measuring to ensure updates are a success</li>
+        <li>Much more!</li>
+      </ul>
+      <form action="submit">
+        <label htmlFor="email">Email address</label>
+        <input type="email" placeholder="Email address" />
+        <button type="submit">Subscribe to monthly newsletter</button>
+      </form>
       Stay updated! Join 60,000+ product managers receiving monthly updates on:
       Product discovery and building what matters Measuring to ensure updates
       are a success And much more! Email address email@company.com Subscribe to
