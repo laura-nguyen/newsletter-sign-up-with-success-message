fb194930e4de922aaeddb5ef6fed0758c60ac5d9
Laura Nguyen <hello@lauranguyen.me>
2024-08-18 23:32:11 -0400

    added tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        tomato: "hsl(4, 100%, 67%)",
+        "dark-slate-grey": "hsl(234, 29%, 20%)",
+        "charcoal-grey": "hsl(235, 18%, 26%)",
+        grey: "hsl(231, 7%, 60%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      breakpoints: {
+        sm: "375px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+      },
     },
   },
   plugins: [],
