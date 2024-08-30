eceb113b024a8e96e9c982d31b1cf90a62e12068
Laura Nguyen <hello@lauranguyen.me>
2024-08-30 00:03:34 -0400

    added confirmation page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,24 @@ export default function Home() {
           </button>
         </form>
       </section>
+      <section>
+        <img
+          src="/assets/images/icon-success.svg"
+          alt=""
+          className="md:block"
+        />
+        <h1>Thanks for subscribing!</h1>
+        <p>
+          A confirmation email has been sent to ash@loremcompany.com. Please
+          open it and click the button inside to confirm your subscription.
+        </p>
+        <button
+          type="submit"
+          className="text-white bg-dark-slate-grey p-4 text-sm rounded-md mt-6 font-semibold"
+        >
+          Dismiss message
+        </button>
+      </section>
       {/* Thanks for subscribing! A confirmation email has been sent to
       ash@loremcompany.com. Please open it and click the button inside to
       confirm your subscription. Dismiss message */}
